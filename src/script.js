3c056c6eceb15aca88726b8036b75d56a5ff30cc
Paulo Paupitz Neto <paupitzneto@gmail.com>
2023-06-14 18:54:16 -0300

    mudanças na pasta de imagens e modificações

--- src/script.js
+++ src/script.js
@@ -1,39 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const btnCadastrar = document.getElementById('btnCadastrar');
-  const popup = document.getElementById('popup');
-  const btnFechar = document.getElementById('btnFechar');
-  const btnRemover = document.getElementById('btnRemover');
+document.addEventListener("DOMContentLoaded", function () {
+  const btnCadastrar = document.getElementById("btnCadastrar");
+  const popup = document.getElementById("popup");
+  const btnSim = document.getElementById("btnSim");
+  const btnCancelar = document.getElementById("btnCancelar");
 
-  btnCadastrar.addEventListener('click', function(event) {
+  btnCadastrar.addEventListener("click", function (event) {
     event.preventDefault();
 
     // Exibir o popup
-    popup.style.display = 'block';
+    popup.style.display = "block";
   });
 
   function fecharPopup() {
     // Ocultar o popup
-    popup.style.display = 'none';
+    popup.style.display = "none";
   }
-  function removerVacina(){
+  function removerVacina() {}
 
-  }
-
-  btnFechar.addEventListener('click', function(event) {
+  btnSim.addEventListener("click", function (event) {
     event.preventDefault();
     fecharPopup();
   });
 
-  btnRemover.addEventListener('click', function(event) {
+  btnCancelar.addEventListener("click", function (event) {
     event.preventDefault();
     removerVacina();
   });
 
-
-
-
-
-  document.addEventListener('click', function(event) {
+  document.addEventListener("click", function (event) {
     // Verificar se o clique ocorreu fora do popup ou no botão "Cadastrar"
     if (!popup.contains(event.target) && event.target !== btnCadastrar) {
       fecharPopup();
